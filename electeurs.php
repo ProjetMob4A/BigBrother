56dc914a68ce8555b767c0244f457db8656dca96
root <root@osboxes>
2017-05-18 07:52:20 +0100

    add setAllA_voteToNull

--- electeurs.php
+++ electeurs.php
@@ -80,4 +80,14 @@ function deleteElecteur($id){
 
 }
 
+// Set tous les booléens à 0
+
+function setAllAVoteToNull(){
+
+  $sql = "UPDATE Electeurs set A_vote = 0";
+
+  $req = mysql_query($sql);
+
+}
+
 ?>
