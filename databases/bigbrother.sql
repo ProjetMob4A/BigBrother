d0aebc7f405b827a22c7aae58ad5ff38568c6808
root <root@osboxes>
2017-05-24 12:00:41 +0100

    databases

--- databases/bigbrother.sql
+++ databases/bigbrother.sql
@@ -29,16 +29,34 @@ CREATE TABLE Certificats (id_cert INT not null, cert VARCHAR (255) not null, PRI
 
 
 
-INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (0, 'Candidat 1', 22);
-INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (1, 'Candidat 2', 41);
-INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (2, 'Candidat 3', 17);
+INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (0, 'Wladimir d\'Ormesson (LR)', 0);
+INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (1, 'Laurent Sorcelle (UDI)', 0);
+INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (3, 'Céline Bézoui (PS)', 0);
+INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (4, 'Marie-Christine Baudouin (PCF)', 0);
+INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (5, 'François Cormier-Bouligeon (LRM)', 0);
+INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (6, 'Jean-René Coueille (FN)', 0);
+INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (7, 'Françoise Pouzet (EELV)', 0);
+INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (8, 'Frédéric Renard (LFI)', 0);
+INSERT INTO Candidats (id_candidat, Nom, Score) VALUES (9, 'Romain Guillaume (UPR)', 0);
 
 INSERT INTO Electeurs (id_electeur, id_token, secret, A_vote) VALUES (0, 0, 'azerty', False);
 INSERT INTO Electeurs (id_electeur, id_token, secret, A_vote) VALUES (1, 0, 'password', True);
 INSERT INTO Electeurs (id_electeur, id_token, secret, A_vote) VALUES (2, 1, 'qwerty', True);
+INSERT INTO Electeurs (id_electeur, id_token, secret, A_vote) VALUES (3, 3, 'poney', False);
+INSERT INTO Electeurs (id_electeur, id_token, secret, A_vote) VALUES (4, 2, '123456', True);
+INSERT INTO Electeurs (id_electeur, id_token, secret, A_vote) VALUES (5, 2, 'dragon', True);
+INSERT INTO Electeurs (id_electeur, id_token, secret, A_vote) VALUES (6, 1, 'spiderman', False);
+INSERT INTO Electeurs (id_electeur, id_token, secret, A_vote) VALUES (7, 0, 'asterix', True);
+INSERT INTO Electeurs (id_electeur, id_token, secret, A_vote) VALUES (8, 3, 'election', True);
 
 INSERT INTO Tokens (id_token, id_cert) VALUES (0,0);
 INSERT INTO Tokens (id_token, id_cert) VALUES (1,1);
+INSERT INTO Tokens (id_token, id_cert) VALUES (2,2);
+INSERT INTO Tokens (id_token, id_cert) VALUES (3,3);
 
-INSERT INTO Certificats (id_cert, cert) VALUES (0, 'n=13373343543&e=65537');
-INSERT INTO Certificats (id_cert, cert) VALUES (1, 'n=4164642&e=3');
+INSERT INTO Certificats (id_cert, cert) VALUES (0, 'n=219&e=5');
+INSERT INTO Certificats (id_cert, cert) VALUES (1, 'n=219&e=5');
+INSERT INTO Certificats (id_cert, cert) VALUES (2, 'n=219&e=5');
+INSERT INTO Certificats (id_cert, cert) VALUES (3, 'n=219&e=5');
+
+
--- databases/web.sql
+++ databases/web.sql
@@ -3,3 +3,12 @@ CREATE TABLE Users (id INT not null, Password VARCHAR (255), PRIMARY KEY (id));
 INSERT INTO Users (id, Password) VALUES (0,"unicorn");
 INSERT INTO Users (id, Password) VALUES (1,"dragon");
 INSERT INTO Users (id, Password) VALUES (2,"zwei");
+INSERT INTO Users (id, Password) VALUES (3,"machin");
+INSERT INTO Users (id, Password) VALUES (4,"truc");
+INSERT INTO Users (id, Password) VALUES (5,"bidule");
+INSERT INTO Users (id, Password) VALUES (6,"etc");
+INSERT INTO Users (id, Password) VALUES (7,"123456");
+INSERT INTO Users (id, Password) VALUES (8,"adrien");
+INSERT INTO Users (id, Password) VALUES (9,"zweisamkeit");
+
+
