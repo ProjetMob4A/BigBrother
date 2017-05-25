c0d5a50bdcbe1efb3c0ec87b1ec87a9d57808922
root <adrien.bouteiller@zoho.com>
2017-05-25 17:13:19 +0100

    adding pass checking feature (to improve!)

--- token.py
+++ token.py
@@ -1,32 +1,48 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # Script client
 
 import socket
-from time import sleep
-import subprocess
+from os import popen
+from hashlib import sha256
 
-host="192.168.1.49"
-port=10000
+hash_pass = sha256('AragonZweisamkeit').hexdigest() # Stocker en dur et checker l'intégrité côté serveur ?
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((host, port))
-print "Connection on {}".format(port)
+salt = "Aragon"
 
-identifiant = socket.recv(255)
-print "Your id is {}".format(identifiant)
+password_in = raw_input("Entrez le mot de passe de votre token : ")
 
-# Quand on aura le .jar :
+hash_in = sha256(salt + password_in).hexdigest()
 
-# cmd = "java -jar token.jar " + identifiant
+if hash_in != hash_pass:
 
-# out = os.popen(cmd)
+	print "Erreur : mot de passe incorrect"
+	exit()
 
-# secret = out.read()
+else:
 
-secret = "35:77:6b:12:a7:a9\n"
-socket.sendall(secret)
-print "Secret sent"
+	host="192.168.1.49"
+	port=10000
 
-print "Close"
-socket.close()
+	socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	socket.connect((host, port))
+	print "Connection on {}".format(port)
+
+	identifiant = socket.recv(255)
+	print "Your id is {}".format(identifiant)
+
+	# Quand on aura le .jar :
+
+	# cmd = "java -jar token.jar " + identifiant
+
+	# out = popen(cmd)
+
+	# secret = out.read()
+
+	secret = "35:77:6b:12:a7:a9\n"
+	socket.sendall(secret)
+	print "Secret sent"
+
+	print "Close"
+	socket.close()
